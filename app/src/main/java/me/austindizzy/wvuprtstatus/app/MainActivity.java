304cf05701b401b54fef2e0028d39c9efb159495
Austin Dizzy <dizzy@wow.com>
2014-07-22 03:06:58 -0400

    Updated Asynchronous logic to include appropriate style and text changes.

--- app/src/main/java/me/austindizzy/wvuprtstatus/app/MainActivity.java
+++ app/src/main/java/me/austindizzy/wvuprtstatus/app/MainActivity.java
@@ -1,18 +1,24 @@
 package me.austindizzy.wvuprtstatus.app;
 
+import android.app.Activity;
+import android.app.Application;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Context;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,34 +26,130 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Date;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("PRT", "CREATED");
+
+        Context context = getApplicationContext();
+        new FetchPRTTask(context).execute("http://prtstatus.sitespace.wvu.edu/cache.php?json=true");
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i("OPTIONS", "CREATED");
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-           Log.i("Selected", " OptionsItem");
         int id = item.getItemId();
         if (id == R.id.action_settings) {
-            Context context = getApplicationContext();
+            Context context = MainActivity.this;
+
+            Toast toast = Toast.makeText(context, "Settings Clicked", Toast.LENGTH_SHORT);
+            toast.show();
             new FetchPRTTask(context).execute("http://prtstatus.sitespace.wvu.edu/cache.php?json=true");
-            getWindow().getDecorView().setBackgroundColor(getResources().getColor(R.color.FireBrick));
         }
         return super.onOptionsItemSelected(item);
     }
+
+    Activity main = this;
+
+    private class FetchPRTTask extends AsyncTask<String, Void, String> {
+
+        private Context mContext;
+
+        public FetchPRTTask(Context context){
+            mContext = context;
+        }
+
+        protected String doInBackground(String... url){
+
+            InputStream is = null;
+            String result = "";
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpGet httpget = new HttpGet(url[0]);
+                HttpResponse response = httpclient.execute(httpget);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+            } catch(Exception e) {
+                Log.i("HTTP", "Caught " + e);
+                return null;
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result = sb.toString();
+                return result;
+            } catch(Exception e) {
+                Log.i("JSON", "Caught " + e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String result) {
+            JSONObject prtObj = null;
+
+            try {
+                prtObj = new JSONObject(result);
+            } catch (JSONException e) {
+                Log.i("JSON EXCEPTION", e.toString());
+            }
+
+            parseStatus(prtObj);
+        }
+
+        public void parseStatus(JSONObject prtObj) {
+            try {
+
+                String prtMessage = prtObj.get("message").toString();
+                int prtStatus = Integer.parseInt(prtObj.get("status").toString());
+                long longPRTDate = Long.parseLong(prtObj.get("timestamp").toString()) * 1000;
+                String prtDate = new Date(longPRTDate).toString();
+
+
+                updateStatus(prtMessage, prtDate, prtStatus);
+
+            } catch (JSONException e) {
+                Log.i("PARSE EXCEPTION", e.toString() + prtObj.toString());
+            }
+
+        }
+
+        public void updateStatus(String prtMessage, String prtDate, int prtStatus){
+
+            TextView prtMessageView = (TextView)main.findViewById(R.id.prtMessage);
+            TextView prtUpdatedView = (TextView)main.findViewById(R.id.updatedTime);
+
+            prtMessageView.setText(prtMessage);
+            prtUpdatedView.setText("Updated " + prtDate);
+
+            Toast toast = Toast.makeText(mContext, prtMessage, Toast.LENGTH_SHORT);
+            toast.show();
+
+            if (prtStatus != 1) {
+                main.getWindow().getDecorView().setBackgroundColor(main.getResources().getColor(R.color.FireBrick));
+            } else {
+                main.getWindow().getDecorView().setBackgroundColor(main.getResources().getColor(R.color.ForestGreen));
+            }
+        }
+    }
+
 }
