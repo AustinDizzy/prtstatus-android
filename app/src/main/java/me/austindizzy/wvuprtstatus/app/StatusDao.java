cee2c70911b3aa098e4fb506e933962000f71bc8
Austin Dizzy <dizzy@wow.com>
2018-03-28 06:07:03 -0400

    Assure updates are chronological by 'timestamp'

--- app/src/main/java/me/austindizzy/wvuprtstatus/app/StatusDao.java
+++ app/src/main/java/me/austindizzy/wvuprtstatus/app/StatusDao.java
@@ -2,21 +2,20 @@ package me.austindizzy.wvuprtstatus.app;
 
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import java.util.List;
 
 @Dao
 public interface StatusDao {
-    @Query("SELECT * FROM updates")
-    List<PRTStatus> getAll();
 
-    @Query("SELECT * FROM updates WHERE timestamp between (:now-(60*60*24*7)) and :now ORDER BY id ASC")
+    @Query("SELECT * FROM updates WHERE timestamp between (:now-(60*60*24*7)) and :now ORDER BY timestamp DESC LIMIT 15")
     List<PRTStatus> getRecent(long now);
 
-    @Query("SELECT * FROM updates ORDER BY id DESC LIMIT 1")
+    @Query("SELECT * FROM updates ORDER BY timestamp DESC LIMIT 1")
     PRTStatus getLast();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(PRTStatus status);
 }
